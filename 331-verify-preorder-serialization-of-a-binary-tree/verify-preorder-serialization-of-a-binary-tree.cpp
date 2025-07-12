d1ff7cf68d747c2776bad76bb4087ccafa32b8cf
Muhammad Hasaam <m.hasaam.official@gmail.com>
2025-07-12 15:13:13 +0500

    Time: 0 ms (100.00%) | Memory: 10 MB (53.14%) - LeetSync

--- 331-verify-preorder-serialization-of-a-binary-tree/verify-preorder-serialization-of-a-binary-tree.cpp
+++ 331-verify-preorder-serialization-of-a-binary-tree/verify-preorder-serialization-of-a-binary-tree.cpp
@@ -1,44 +1,69 @@
+// 0ms CORRECT SOLUTION
+
+// class Solution {
+// public:
+//     void test(const vector<int>& nodes, int& idx, const int& size) {
+
+//         if (idx >= size) {
+//             idx += 2;
+//             return;
+//         }
+
+//         if (nodes[idx] != -1) {
+
+//             idx++;
+
+//             test(nodes, idx, size);
+//             test(nodes, idx, size);
+
+//         } else {
+
+//             idx++;
+//         }
+//     }
+
+//     bool isValidSerialization(string preorder) {
+
+//         stringstream ss(preorder);
+
+//         string token;
+
+//         vector<int> nodes;
+//         while (getline(ss, token, ',')) {
+//             if (token == "#")
+//                 nodes.push_back(-1);
+//             else
+//                 nodes.push_back(stoi(token));
+//         }
+
+//         int idx = 0;
+//         int size = nodes.size();
+
+//         test(nodes, idx, size);
+
+//         return idx == size;
+//     }
+// };
+
 class Solution {
 public:
-    void test(const vector<int>& nodes, int& idx, const int& size) {
-
-        if (idx >= size) {
-            idx += 2;
-            return;
-        }
-
-        if (nodes[idx] != -1) {
-
-            idx++;
-
-            test(nodes, idx, size);
-            test(nodes, idx, size);
-
-        } else {
-
-            idx++;
-        }
-    }
-
     bool isValidSerialization(string preorder) {
-
         stringstream ss(preorder);
 
         string token;
 
-        vector<int> nodes;
+        int slot = 1;
+
         while (getline(ss, token, ',')) {
-            if (token == "#")
-                nodes.push_back(-1);
-            else
-                nodes.push_back(stoi(token));
+            slot--;
+
+            if (slot < 0)
+                return false;
+
+            if (token != "#")
+                slot += 2;
         }
 
-        int idx = 0;
-        int size = nodes.size();
-
-        test(nodes, idx, size);
-
-        return idx == size;
+        return slot == 0;
     }
 };
