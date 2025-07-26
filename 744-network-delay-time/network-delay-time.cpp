06640e1d3020d8cf88f5a70699e6be2c00007dd5
Muhammad Hasaam <m.hasaam.official@gmail.com>
2025-07-26 13:27:05 +0500

    Time: 92 ms (35.31%) | Memory: 44.4 MB (68.44%) - LeetSync

---
+++ 744-network-delay-time/network-delay-time.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        using P = pair<int, int>; // next , time
+
+        vector<vector<P>> graph(n + 1); // 1-based indexing
+
+        for (const vector<int>& time : times) {
+            int u = time[0], v = time[1], w = time[2];
+            graph[u].emplace_back(v, w);
+        }
+
+        vector<int> visited(n + 1, INT_MAX); // 1-based indexing
+        visited[k] = 0;
+
+        queue<int> q;
+        q.push(k);
+
+        int Max = 0;
+
+        while (!q.empty()) {
+            int front = q.front();
+            q.pop();
+
+            for (auto& [next, weight] : graph[front]) {
+                if (visited[next] > visited[front] + weight) {
+                    visited[next] = visited[front] + weight;
+                    q.push(next);
+                }
+            }
+        }
+
+        for (int i = 1; i <= n; ++i) {
+            if (visited[i] == INT_MAX)
+                return -1;
+            Max = max(Max, visited[i]);
+        }
+
+        return Max;
+    }
+};
