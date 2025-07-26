1592c870a4d1d05352b2aba6a1c558f431047d02
Muhammad Hasaam <m.hasaam.official@gmail.com>
2025-07-26 13:46:36 +0500

    Time: 75 ms (88.58%) | Memory: 44.4 MB (56.12%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -1,4 +1,6 @@
-class Solution {
+
+// using BFS 92 ms
+class Solution2 {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         using P = pair<int, int>; // next , time
@@ -34,6 +36,56 @@ public:
             if (visited[i] == INT_MAX)
                 return -1;
             Max = max(Max, visited[i]);
+        }
+
+        return Max;
+    }
+};
+
+// using Priority BFS
+class Solution {
+public:
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        using P = pair<int, int>;
+
+        // P = ( next , time )
+        vector<vector<P>> graph(n + 1); // 1-based indexing
+
+        for (const vector<int>& time : times) {
+            int u = time[0], v = time[1], w = time[2];
+            graph[u].emplace_back(v, w);
+        }
+
+        vector<int> min_time(n + 1, INT_MAX);        // 1-based indexing
+        priority_queue<P, vector<P>, greater<P>> pq; // P = ( time , src )
+
+        min_time[k] = 0;
+        pq.emplace(0, k);
+
+        while (!pq.empty()) {
+
+            auto [curr_time, front] = pq.top();
+            pq.pop();
+
+            if (curr_time > min_time[front])
+                continue;
+
+            for (auto& [next, time] : graph[front]) {
+
+                if (min_time[next] > time + curr_time) {
+                    min_time[next] = time + curr_time;
+
+                    pq.emplace(min_time[next], next);
+                }
+            }
+        }
+
+        int Max = 0;
+
+        for (int i = 1; i <= n; ++i) {
+            if (min_time[i] == INT_MAX)
+                return -1;
+            Max = max(Max, min_time[i]);
         }
 
         return Max;
