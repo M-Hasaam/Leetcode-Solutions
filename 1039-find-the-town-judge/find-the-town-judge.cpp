b8be154e227c7da4f17f2b92639028a1794e0e6a
Muhammad Hasaam <m.hasaam.official@gmail.com>
2025-07-29 23:30:28 +0500

    Time: 16 ms (44.04%) | Memory: 70.3 MB (42.17%) - LeetSync

---
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+
+        vector<vector<int>> graph(n + 1);
+
+        vector<int> Trust_factor(n + 1, 0);
+
+        for (const auto& A_B : trust) {
+            graph[A_B[0]].emplace_back(A_B[1]);
+            Trust_factor[A_B[1]]++;
+        }
+
+        for (int i = 1; i <= n; i++)
+            if (Trust_factor[i] == n - 1 && graph[i].size() == 0)
+                return i;
+
+        return -1;
+    }
+};
