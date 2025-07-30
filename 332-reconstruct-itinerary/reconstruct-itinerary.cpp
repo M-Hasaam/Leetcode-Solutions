322b67475aae029d8046244fd5d900c51830e293
Muhammad Hasaam <m.hasaam.official@gmail.com>
2025-07-30 14:18:46 +0500

    Time: 14 ms (14.67%) | Memory: 22.1 MB (19.52%) - LeetSync

---
+++ 332-reconstruct-itinerary/reconstruct-itinerary.cpp
@@ -0,0 +1,33 @@
+class Solution {
+    using pq = priority_queue<string, vector<string>, greater<string>>;
+
+    unordered_map<string, pq> graph;
+    vector<string> answer;
+
+    void dfs(string airport) {
+
+        while (!graph[airport].empty()) {
+
+            string next_airport = graph[airport].top();
+            graph[airport].pop();
+
+            dfs(next_airport);
+        }
+
+        answer.push_back(airport);
+    }
+
+public:
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+
+        for (auto ticket : tickets) {
+            graph[ticket[0]].push(ticket[1]);
+        }
+
+        dfs("JFK");
+
+        reverse(answer.begin(), answer.end());
+
+        return answer;
+    }
+};
