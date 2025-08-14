c882c77728a7140a207adad65ad93799ca3dce0b
Muhammad Hasaam <m.hasaam.official@gmail.com>
2025-08-14 10:20:26 +0500

    Time: 2 ms (66.51%) | Memory: 9.1 MB (14.63%) - LeetSync

---
+++ 326-power-of-three/power-of-three.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+
+        if (n < 1)
+            return false;
+
+        long long int i = 1;
+        while (i < n) {
+            i *= 3;
+        }
+        return i == n;
+    }
+};
