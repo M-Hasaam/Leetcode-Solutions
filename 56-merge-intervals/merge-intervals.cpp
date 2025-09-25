da77555b81c0fb1eb07ad2fffa1bf5d31e37f4fd
Muhammad Hasaam <m.hasaam.official@gmail.com>
2025-09-26 01:13:42 +0500

    Time: 349 ms (5.20%) | Memory: 23.6 MB (94.10%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+
+        vector<vector<int>> ans;
+
+        int n = intervals.size();
+
+        for (int i = 0; i < n; i++) {
+
+            // int start = -1, end = -1;
+
+            int start_i = intervals[i][0], end_i = intervals[i][1];
+
+            if (start_i == -1 || end_i == -1)
+                continue;
+
+            for (int j = i + 1; j < n; j++) {
+                int start_j = intervals[j][0], end_j = intervals[j][1];
+
+                if (start_j == -1 || end_j == -1)
+                    continue;
+
+                if ((start_i <= start_j && start_j <= end_i) ||
+                    (start_i <= end_j && end_j <= end_i) ||
+                    (start_j <= start_i && start_i <= end_j) ||
+                    (start_j <= end_i && end_i <= end_j)) {
+
+                    start_i = min(start_i, start_j);
+                    end_i = max(end_i, end_j);
+
+                    intervals[j][0] = -1;
+                    intervals[j][1] = -1;
+
+                    j = i ;
+                }
+            }
+
+            ans.push_back({start_i, end_i});
+        }
+
+        return ans;
+    }
+};
