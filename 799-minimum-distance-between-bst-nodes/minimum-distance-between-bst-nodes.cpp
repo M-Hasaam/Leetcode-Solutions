e989791a47c2ccecfd30e899785b4718537b957d
Muhammad Hasaam <m.hasaam.official@gmail.com>
2025-09-30 09:24:11 +0500

    Time: 0 ms (100.00%) | Memory: 12.4 MB (86.17%) - LeetSync

---
+++ 799-minimum-distance-between-bst-nodes/minimum-distance-between-bst-nodes.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int MIN = INT_MAX;
+    TreeNode* pre = nullptr;
+
+    int minDiffInBST(TreeNode* root) {
+        MIN = INT_MAX;
+        pre = nullptr;
+
+        rec(root);
+
+        return MIN;
+    }
+
+    void rec(TreeNode* root) {
+
+        if (!root)
+            return;
+
+        rec(root->left);
+
+        if (pre) {
+            MIN = min(MIN, abs(root->val - pre->val));
+        }
+        pre = root;
+
+        rec(root->right);
+    }
+};
