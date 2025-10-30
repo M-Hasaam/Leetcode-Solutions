e513f0c2e24bfdb4740c7150cb35876f8d9f7eb6
Muhammad Hasaam <m.hasaam.official@gmail.com>
2025-10-31 00:45:06 +0500

    Time: 0 ms (100.00%) | Memory: 37.2 MB (79.43%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
@@ -13,6 +13,37 @@
 class Solution {
 public:
     bool findTarget(TreeNode* root, int k) {
+        vector<int> inorder_Array;
+        inorder(root, inorder_Array);
+
+        int l = 0, r = inorder_Array.size() - 1;
+
+        while (l < r) {
+            int sum = inorder_Array[l] + inorder_Array[r];
+            if (sum == k)
+                return true;
+            else if (sum < k)
+                l++;
+            else if (sum > k)
+                r--;
+        }
+
+        return false;
+    }
+
+private:
+    void inorder(TreeNode* node, vector<int>& inorder_Array) {
+        if (node) {
+            inorder(node->left,inorder_Array);
+            inorder_Array.push_back(node->val);
+            inorder(node->right,inorder_Array);
+        }
+    }
+};
+
+class Solution_DFS {
+public:
+    bool findTarget(TreeNode* root, int k) {
         unordered_set<int> SET;
         return dfs(root, k, SET);
     }
