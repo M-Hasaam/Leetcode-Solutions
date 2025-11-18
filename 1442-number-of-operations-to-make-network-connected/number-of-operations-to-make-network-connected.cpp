64a7e6f5b00291925ee3e1e43ff9c29e335c6256
Muhammad Hasaam <m.hasaam.official@gmail.com>
2025-11-18 23:52:01 +0500

    Time: 19 ms (56.84%) | Memory: 47.7 MB (30.05%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -1,4 +1,49 @@
 class Solution {
+public:
+    int makeConnected(int n, vector<vector<int>>& connections) {
+
+        if (connections.size() < n - 1)
+            return -1;
+
+        vector<int> parent(n);
+        for (int i = 0; i < n; i++)
+            parent[i] = i;
+        vector<int> size(n, 1);
+
+        function<int(int)> find = [&](int a) {
+            if (parent[a] != a)
+                parent[a] = find(parent[a]);
+            return parent[a];
+        };
+
+        auto unite = [&](int a, int b) {
+            int pa = find(a);
+            int pb = find(b);
+
+            if (pa == pb)
+                return;
+
+            if (size[pa] < size[pb])
+                swap(pa, pb);
+
+            parent[pb] = pa;
+            size[pa] += size[pb];
+        };
+
+        for (auto con : connections)
+            unite(con[0], con[1]);
+
+        int components = 0;
+
+        for (int i = 0; i < n; i++)
+            if (parent[i] == i)
+                components++;
+
+        return components - 1;
+    }
+};
+
+class Solution_64ms {
 
 public:
     int makeConnected(int n, vector<vector<int>>& connections) {
