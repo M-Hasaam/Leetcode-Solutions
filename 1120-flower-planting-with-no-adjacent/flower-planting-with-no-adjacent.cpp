46008a4eef08892847d33bcbfeb838091b03c0a2
Muhammad Hasaam <m.hasaam.official@gmail.com>
2025-12-11 01:34:34 +0500

    Time: 51 ms (46.23%) | Memory: 48.1 MB (57.92%) - LeetSync

--- 1120-flower-planting-with-no-adjacent/flower-planting-with-no-adjacent.cpp
+++ 1120-flower-planting-with-no-adjacent/flower-planting-with-no-adjacent.cpp
@@ -9,6 +9,40 @@ public:
             graph[p[1]].push_back(p[0]);
         }
 
+        vector<int> color(n, 0);
+
+        for (int i = 0; i < n; i++) {
+
+            bool used[4] = {false};
+
+            for (int v : graph[i + 1]) {
+                if (color[v - 1] != 0)
+                    used[color[v - 1] - 1] = true;
+            }
+
+            for (int j = 0; j < 4; j++) {
+                if (!used[j]) {
+                    color[i] = j + 1;
+                    break;
+                }
+            }
+        }
+
+        return color;
+    }
+};
+
+class Solution_BFS {
+public:
+    vector<int> gardenNoAdj(int n, vector<vector<int>>& paths) {
+
+        vector<vector<int>> graph(n + 1);
+
+        for (auto p : paths) {
+            graph[p[0]].push_back(p[1]);
+            graph[p[1]].push_back(p[0]);
+        }
+
         vector<int> visited(n, 0);
 
         for (int i = 1; i <= n; i++) {
