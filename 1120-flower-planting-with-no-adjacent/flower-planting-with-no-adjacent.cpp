71af64980b28bea797ff13a54e9a859c2ff49445
Muhammad Hasaam <m.hasaam.official@gmail.com>
2025-12-11 01:17:22 +0500

    Time: 187 ms (7.79%) | Memory: 103.1 MB (5.06%) - LeetSync

---
+++ 1120-flower-planting-with-no-adjacent/flower-planting-with-no-adjacent.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    vector<int> gardenNoAdj(int n, vector<vector<int>>& paths) {
+
+        vector<vector<int>> graph(n + 1);
+
+        for (auto p : paths) {
+            graph[p[0]].push_back(p[1]);
+            graph[p[1]].push_back(p[0]);
+        }
+
+        vector<int> visited(n, 0);
+
+        for (int i = 1; i <= n; i++) {
+            if (visited[i - 1] == 0) {
+                visited[i - 1] = 1;
+
+                queue<int> q;
+                q.push(i);
+
+                while (!q.empty()) {
+                    int curr = q.front();
+                    q.pop();
+
+                    // if (visited[i - 1])
+                    //     continue;
+
+                    bool n4[4] = {false};
+
+                    for (int v : graph[curr]) {
+                        if (visited[v - 1] != 0) {
+                            n4[visited[v - 1] - 1] = true;
+                        } else {
+                            q.push(v);
+                        }
+                    }
+
+                    for (int j = 0; j < 4; j++) {
+                        if (!n4[j]) {
+                            visited[curr - 1] = j + 1;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        return visited;
+    }
+};
