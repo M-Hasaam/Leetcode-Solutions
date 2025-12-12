ee47fd4b15b59f2fa2fb370f80e79ae73435fe28
Muhammad Hasaam <m.hasaam.official@gmail.com>
2025-12-13 02:05:44 +0500

    Time: 36 ms (73.68%) | Memory: 72.8 MB (44.36%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.cpp
+++ 1558-course-schedule-iv/course-schedule-iv.cpp
@@ -7,38 +7,47 @@ public:
         int n = numCourses;
 
         vector<vector<int>> graph(n);
+        vector<int> indeg(n, 0);
 
         for (auto p : prerequisites) {
             int u = p[0], v = p[1];
             graph[u].push_back(v);
+            indeg[v]++;
         }
 
-        int m = queries.size();
-
-        vector<bool> ans(m, false);
-
-        auto dfs = [&](int u, int v) {
+        vector<vector<bool>> reachable(n, vector<bool>(n, false));
+        
+        {
             queue<int> q;
-            vector<bool> visited(n, false);
-            q.push(u);
-            visited[u] = true;
+
+            for (int i = 0; i < n; i++)
+                if (indeg[i] == 0)
+                    q.push(i);
+
             while (!q.empty()) {
                 int curr = q.front();
                 q.pop();
-                for (int nxt : graph[curr])
-                    if (!visited[nxt]) {
-                        if (nxt == v)
-                            return true;
-                        visited[nxt] = true;
-                        q.push(nxt);
-                    }
-            }
-            return false;
-        };
 
-        for (int i = 0; i < m; i++) {
-            ans[i] = dfs(queries[i][0], queries[i][1]);
+                for (int nxt : graph[curr]) {
+                    indeg[nxt]--;
+
+                    reachable[curr][nxt] = true;
+
+                    for (int i = 0; i < n; i++)
+                        if (reachable[i][curr])
+                            reachable[i][nxt] = true;
+
+                    if (indeg[nxt] == 0)
+                        q.push(nxt);
+                }
+            }
         }
+
+        vector<bool> ans;
+        ans.reserve(n);
+
+        for (auto q : queries)
+            ans.push_back(reachable[q[0]][q[1]]);
 
         return ans;
     }
