100c8a59f5a74aa2e858d91210bf6d8513791a62
Muhammad Hasaam <m.hasaam.official@gmail.com>
2025-12-13 02:11:26 +0500

    Time: 62 ms (50.34%) | Memory: 72.7 MB (47.44%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.cpp
+++ 1558-course-schedule-iv/course-schedule-iv.cpp
@@ -16,7 +16,7 @@ public:
         }
 
         vector<vector<bool>> reachable(n, vector<bool>(n, false));
-        
+
         {
             queue<int> q;
 
@@ -52,6 +52,44 @@ public:
         return ans;
     }
 };
+class Solution_DFS_194ms {
+public:
+    vector<bool> checkIfPrerequisite(int numCourses,
+                                     vector<vector<int>>& prerequisites,
+                                     vector<vector<int>>& queries) {
+        int n = numCourses;
+        vector<vector<int>> graph(n);
+        for (auto p : prerequisites) {
+            int u = p[0], v = p[1];
+            graph[u].push_back(v);
+        }
+        int m = queries.size();
+        vector<bool> ans(m, false);
+        auto dfs = [&](int u, int v) {
+            queue<int> q;
+            vector<bool> visited(n, false);
+            q.push(u);
+            visited[u] = true;
+            while (!q.empty()) {
+                int curr = q.front();
+                q.pop();
+                for (int nxt : graph[curr])
+                    if (!visited[nxt]) {
+                        if (nxt == v)
+                            return true;
+                        visited[nxt] = true;
+                        q.push(nxt);
+                    }
+            }
+            return false;
+        };
+        for (int i = 0; i < m; i++) {
+            ans[i] = dfs(queries[i][0], queries[i][1]);
+        }
+        return ans;
+    }
+};
+
 class Solution_2084ms {
 public:
     vector<bool> checkIfPrerequisite(int numCourses,
