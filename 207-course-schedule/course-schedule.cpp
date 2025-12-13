1d121b000a71e81bc1e25e16820c5a35c66e7d2f
Muhammad Hasaam <m.hasaam.official@gmail.com>
2025-12-14 00:46:16 +0500

    Time: 1 ms (85.01%) | Memory: 19.1 MB (83.30%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -2,6 +2,41 @@ class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
 
+        vector<vector<int>> graph(numCourses);
+        vector<int> indegree(numCourses, 0);
+
+        for (auto& p : prerequisites) {
+            graph[p[1]].push_back(p[0]);
+            indegree[p[0]]++;
+        }
+
+        queue<int> q;
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0)
+                q.push(i);
+        }
+
+        int count = 0;
+
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+            count++;
+
+            for (int v : graph[u]) {
+                if (--indegree[v] == 0)
+                    q.push(v);
+            }
+        }
+
+        return count == numCourses;
+    }
+};
+
+class Solution_DFS_14ms {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+
         int n = numCourses;
         vector<vector<int>> graph(n);
 
