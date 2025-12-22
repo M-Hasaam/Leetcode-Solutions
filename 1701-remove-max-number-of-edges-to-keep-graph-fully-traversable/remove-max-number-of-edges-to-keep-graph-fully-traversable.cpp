57ff10f0523cd1cc1efd351f5161d305825b34cd
Muhammad Hasaam <m.hasaam.official@gmail.com>
2025-12-22 13:36:09 +0500

    Time: 153 ms (20.97%) | Memory: 176.2 MB (19.07%) - LeetSync

--- 1701-remove-max-number-of-edges-to-keep-graph-fully-traversable/remove-max-number-of-edges-to-keep-graph-fully-traversable.cpp
+++ 1701-remove-max-number-of-edges-to-keep-graph-fully-traversable/remove-max-number-of-edges-to-keep-graph-fully-traversable.cpp
@@ -1,4 +1,69 @@
 class Solution {
+    class DSU {
+    public:
+        vector<int> parent, size;
+        int components;
+
+        DSU(int n) : parent(n + 1), size(n + 1, 1), components(n) {
+            iota(parent.begin(), parent.end(), 0);
+        }
+
+        int find(int x) {
+            if (parent[x] != x)
+                parent[x] = find(parent[x]);
+            return parent[x];
+        }
+
+        bool unite(int a, int b) {
+            a = find(a);
+            b = find(b);
+
+            if (a == b)
+                return false;
+
+            if (b > a)
+                swap(a, b);
+
+            parent[b] = a;
+            components--;
+            size[a] += size[b];
+
+            return true;
+        }
+    };
+
+public:
+    int maxNumEdgesToRemove(int n, vector<vector<int>>& edges) {
+
+        DSU A(n), B(n);
+        int used = 0;
+
+        for (auto e : edges) {
+            if (e[0] == 3) {
+                int u = e[1], v = e[2];
+                if (A.unite(u, v) && B.unite(u, v))
+                    used++;
+            }
+        }
+        for (auto e : edges) {
+            if (e[0] != 3) {
+                int u = e[1], v = e[2];
+
+                if (e[0] == 1 && A.unite(u, v))
+                    used++;
+                if (e[0] == 2 && B.unite(u, v))
+                    used++;
+            }
+        }
+
+        if (A.components != 1 || B.components != 1)
+            return -1;
+
+        return edges.size() - used;
+    }
+};
+
+class Solution_342ms {
 public:
     int maxNumEdgesToRemove(int n, vector<vector<int>>& edges) {
 
