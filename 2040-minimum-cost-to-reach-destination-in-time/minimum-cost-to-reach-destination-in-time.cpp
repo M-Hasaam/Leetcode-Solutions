58cb970d5a61a28a54803048d0ca4d914ec2c1bf
Muhammad Hasaam <m.hasaam.official@gmail.com>
2025-12-26 02:28:17 +0500

    Time: 1045 ms (18.14%) | Memory: 118.7 MB (28.97%) - LeetSync

--- 2040-minimum-cost-to-reach-destination-in-time/minimum-cost-to-reach-destination-in-time.cpp
+++ 2040-minimum-cost-to-reach-destination-in-time/minimum-cost-to-reach-destination-in-time.cpp
@@ -4,62 +4,52 @@ public:
         int u;
         int cost;
         int time;
-        node(int a =0,  int b=0 , int c=0) : u(a) , cost(b) , time(c) {}
+        node(int a = 0, int b = 0, int c = 0) : u(a), cost(b), time(c) {}
     };
 
+    int minCost(int maxTime, vector<vector<int>>& edges,
+                vector<int>& passingFees) {
 
-
-    int minCost(int maxTime, vector<vector<int>>& edges, vector<int>& passingFees) {
-        
         auto cmp = [](const node& a, const node& b) {
             if (a.cost == b.cost)
-                return a.time > b.time;   // min time
-            return a.cost > b.cost;       // min cost
+                return a.time > b.time; // min time
+            return a.cost > b.cost;     // min cost
         };
-        int numberOfStops = passingFees.size();
+        int n = passingFees.size();
 
-        vector<vector<pair<int,int>>>graph(numberOfStops );
-        for (auto& edge : edges){
-            graph[edge[0]].push_back({edge[1],edge[2]});
-            graph[edge[1]].push_back({edge[0],edge[2]});
+        vector<vector<pair<int, int>>> graph(n);
+        for (auto& e : edges) {
+            graph[e[0]].push_back({e[1], e[2]});
+            graph[e[1]].push_back({e[0], e[2]});
         }
 
-        // for (int i = 0 ; i < numberOfStops ; i++){
-        //     cout << i << "-> ";
-        //     for (auto& list: graph[i])
-        //         cout << "{"<< list.first  << "," << list.second << "} ";
-        //     cout << endl;
-        // }
+        priority_queue<node, vector<node>, decltype(cmp)> pq(cmp);
 
+        vector<vector<int>> best(n, vector<int>(maxTime + 1, INT_MAX));
 
-        priority_queue<node, vector<node>, decltype(cmp)> qu(cmp);
-        vector<int>visited(numberOfStops , 0);
-
-        vector<vector<int>> best(numberOfStops, vector<int>(maxTime + 1, INT_MAX));
-        
         best[0][0] = passingFees[0];
-        qu.push({0, passingFees[0], 0});
+        pq.push({0, passingFees[0], 0});
 
-        while (!qu.empty()) {
-            node temp = qu.top();
-            qu.pop();
+        while (!pq.empty()) {
+            node curr = pq.top();
+            pq.pop();
 
-            if (temp.u == numberOfStops - 1)
-                return temp.cost;
+            if (curr.u == n - 1)
+                return curr.cost;
 
-            for (auto& [v, w] : graph[temp.u]) {
-                int newTime = temp.time + w;
-                if (newTime > maxTime) continue;
+            for (auto& [nxt, t] : graph[curr.u]) {
+                int newTime = curr.time + t;
+                if (newTime > maxTime)
+                    continue;
 
-                int newCost = temp.cost + passingFees[v];
+                int newCost = curr.cost + passingFees[nxt];
 
-                if (newCost < best[v][newTime]) {
-                    best[v][newTime] = newCost;
-                    qu.push({v, newCost, newTime});
+                if (newCost < best[nxt][newTime]) {
+                    best[nxt][newTime] = newCost;
+                    pq.push({nxt, newCost, newTime});
                 }
             }
         }
-
 
         return -1;
     }
